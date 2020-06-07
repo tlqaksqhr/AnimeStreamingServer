434e139667f2949d6ef51562c619ed8bc5e2de43
sim manseop <tlqaksqhr@naver.com>
2020-06-07 12:23:31 +0900

    add favorite, review repository

---
+++ src/main/java/com/anime/streamingserver/repository/anime/FavoriteRepository.java
@@ -0,0 +1,18 @@
+package com.anime.streamingserver.repository.anime;
+
+
+import com.anime.streamingserver.entity.anime.Animation;
+import com.anime.streamingserver.entity.anime.Favorite;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+    /*
+    void addFavorite(Favorite favorite);
+    void deleteFavorite(Favorite favorite);
+    int countFavoriteByAnimation(Animation animation);
+
+     */
+}
---
+++ src/main/java/com/anime/streamingserver/repository/anime/ReviewRepository.java
@@ -0,0 +1,19 @@
+package com.anime.streamingserver.repository.anime;
+
+
+import com.anime.streamingserver.entity.anime.Animation;
+import com.anime.streamingserver.entity.anime.Review;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ReviewRepository {
+    /*
+    void findAllReviewByAnimation(Animation animation);
+    void pagingReviewByAnimation(Animation animation);
+    void deleteReviewByAnimation(Review review);
+    void saveReviewByAnimation(Review review);
+    int countReviewByAnimation(Animation animation);
+
+     */
+}
