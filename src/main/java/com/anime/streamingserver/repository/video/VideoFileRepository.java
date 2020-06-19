2b6183acbbae05413228e8dbc71946fc861038ec
sim manseop <tlqaksqhr@naver.com>
2020-06-19 22:53:00 +0900

    add video repository

---
+++ src/main/java/com/anime/streamingserver/repository/video/VideoFileRepository.java
@@ -0,0 +1,4 @@
+package com.anime.streamingserver.repository.video;
+
+public interface VideoFileRepository {
+}
--- src/main/java/com/anime/streamingserver/repository/video/VideoRepository.java
+++ src/main/java/com/anime/streamingserver/repository/video/VideoRepository.java
@@ -3,9 +3,17 @@ package com.anime.streamingserver.repository.video;
 
 import com.anime.streamingserver.entity.anime.Animation;
 import com.anime.streamingserver.entity.video.VideoInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface VideoRepository {
-    List<VideoInfo> findAllVideoByAnimation(Animation animation);
+
+@Repository
+public interface VideoRepository extends JpaRepository<VideoInfo, Long> {
+
+    @Query("SELECT v FROM Video v WHERE animation_id = :animationID")
+    List<VideoInfo> findAllVideoByAnimationID(Long animationID);
 }
