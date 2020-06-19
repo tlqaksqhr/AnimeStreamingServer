8764ac1e46fc9cbb7e3bb2aef5a4d91a28faadbd
sim manseop <tlqaksqhr@naver.com>
2020-06-19 22:56:28 +0900

    add user repository

--- src/main/java/com/anime/streamingserver/repository/user/AdminRepository.java
+++ src/main/java/com/anime/streamingserver/repository/user/AdminRepository.java
@@ -1,4 +1,14 @@
 package com.anime.streamingserver.repository.user;
 
-public interface AdminRepository {
+import com.anime.streamingserver.entity.user.Admin;
+import com.anime.streamingserver.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin,Long> {
+    Optional<Admin> findByEmail(String email);
+    Boolean existsByEmail(String email);
 }
--- src/main/java/com/anime/streamingserver/repository/user/UserRepository.java
+++ src/main/java/com/anime/streamingserver/repository/user/UserRepository.java
@@ -1,4 +1,14 @@
 package com.anime.streamingserver.repository.user;
 
-public interface UserRepository {
+import com.anime.streamingserver.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+    Boolean existsByEmail(String email);
 }
