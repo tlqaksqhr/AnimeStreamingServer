df50c87f2057a1175ad4fcf892cf135d645e6bed
sim manseop <tlqaksqhr@naver.com>
2020-06-19 22:58:38 +0900

    add configuration class into AnimeStreamingServer

--- src/main/java/com/anime/streamingserver/AnimeStreamingServer.java
+++ src/main/java/com/anime/streamingserver/AnimeStreamingServer.java
@@ -1,10 +1,15 @@
 package com.anime.streamingserver;
 
+import com.anime.streamingserver.config.AppProperties;
+import com.anime.streamingserver.service.video.configuration.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+    FileStorageProperties.class, AppProperties.class
+})
 public class AnimeStreamingServer {
 
     public static void main(String[] args) {
