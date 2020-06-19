8c046b9aef9074b3f2d6e75b0a9d77742dd6864c
sim manseop <tlqaksqhr@naver.com>
2020-06-19 22:54:07 +0900

    add anime service

--- src/main/java/com/anime/streamingserver/service/anime/AnimeService.java
+++ src/main/java/com/anime/streamingserver/service/anime/AnimeService.java
@@ -1,22 +1,17 @@
 package com.anime.streamingserver.service.anime;
 
+import com.anime.streamingserver.dto.anime.AnimeDto;
 import com.anime.streamingserver.dto.anime.NewAnimeDto;
 import com.anime.streamingserver.dto.anime.NewAnimeListDto;
 import com.anime.streamingserver.dto.anime.UploadAnimeDataDto;
-import com.anime.streamingserver.entity.anime.Animation;
-import com.anime.streamingserver.entity.anime.Genre;
-import com.anime.streamingserver.entity.anime.Review;
-import com.anime.streamingserver.entity.anime.Tags;
-import com.anime.streamingserver.entity.user.User;
+import com.anime.streamingserver.entity.anime.*;
 import com.anime.streamingserver.repository.anime.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.WeakHashMap;
+import java.util.*;
 
 
 @Service
@@ -32,7 +27,7 @@ public class AnimeService {
     private GenreCategoryRepository genreCategoryRepository;
 
     @Autowired
-    private TagsRepository tagsRepository;
+    private TagRepository tagRepository;
 
     @Autowired
     private TagCategoryRepository tagCategoryRepository;
@@ -104,11 +99,41 @@ public class AnimeService {
         return datePair;
     }
 
+    public AnimeDto getAnimationById(Long id) {
+        AnimeDto animeDto = new AnimeDto();
+
+        Optional<Animation> animationOptional = animationRepository.findById(id);
+
+        Animation animationItem = animationOptional.orElseThrow(NoSuchElementException::new);
+
+        animeDto.setId(animationItem.getId());
+        animeDto.setTitle(animationItem.getTitle());
+        animeDto.setContent(animationItem.getContent());
+        animeDto.setCreateTime(animationItem.getCreateTime());
+        animeDto.setUpdateTime(animationItem.getUpdateTime());
+        animeDto.setThumbnail(animationItem.getThumbnail());
+        animeDto.setAge(animationItem.getAge());
+        animeDto.setMakeInfo(animationItem.getMakeInfo());
+        animeDto.setGenreCategories(animationItem.getGenreCategories());
+        animeDto.setTagCategories(animationItem.getTagCategories());
+
+
+        return animeDto;
+    }
+
 
     // TODO : 나중에 명령(업데이트, 삽입, 삭제) 관련 로직들만 별도의 서비스로 분리하기
 
+    @Transactional
     public void addAnimation(UploadAnimeDataDto uploadAnimeDataDto){
+        insertAnimation(uploadAnimeDataDto);
+    }
 
+    public void updateAnimation(UploadAnimeDataDto uploadAnimeDataDto){
+        insertAnimation(uploadAnimeDataDto);
+    }
+
+    private void insertAnimation(UploadAnimeDataDto uploadAnimeDataDto) {
         Animation animation = new Animation();
 
         animation.setTitle(uploadAnimeDataDto.getTitle());
@@ -117,19 +142,56 @@ public class AnimeService {
         animation.setAge(uploadAnimeDataDto.getAge());
         animation.setThumbnail(uploadAnimeDataDto.getThumbnail());
 
+        List<Genre> genres = new ArrayList<Genre>();
+        List<GenreCategory> genreCategories = new ArrayList<GenreCategory>();
+        List<Tag> tags = new ArrayList<Tag>();
+        List<TagCategory> tagCategories = new ArrayList<TagCategory>();
 
 
+        for(String genreName : uploadAnimeDataDto.getGenres()) {
+            GenreCategory genreCategory = new GenreCategory();
+
+            Genre genre = new Genre();
+            genre.setGenre(genreName);
+
+            genreCategory.setGenre(genre);
+            genreCategory.getGenre().getGenreCategories().add(genreCategory);
+
+            genreCategory.setAnimation(animation);
+            genreCategory.getAnimation().getGenreCategories().add(genreCategory);
+
+            genres.add(genre);
+            genreCategories.add(genreCategory);
+        }
+
+        for(String tagName : uploadAnimeDataDto.getTags()) {
+            TagCategory tagCategory = new TagCategory();
+
+            Tag tag = new Tag();
+            tag.setTag(tagName);
+
+
+            tagCategory.setTag(tag);
+            tagCategory.getTag().getTagCategories().add(tagCategory);
+
+            tagCategory.setAnimation(animation);
+            tagCategory.getAnimation().getTagCategories().add(tagCategory);
+
+            tags.add(tag);
+            tagCategories.add(tagCategory);
+        }
+
+        genreRepository.saveAll(genres);
+        tagRepository.saveAll(tags);
+
+        genreCategoryRepository.saveAll(genreCategories);
+        tagCategoryRepository.saveAll(tagCategories);
 
         animationRepository.save(animation);
     }
 
-    public void deleteAnimation(Animation animation){
-
-        animationRepository.delete(animation);
-
-    }
-
-    public void updateAnimation(Animation animation){
-
+    public void deleteAnimation(Long id){
+        // TODO : GenreCategory, TagCategory 에서 삭제한 애니메이션 id가 포함된 row들 제거하는 부분 추가.
+        animationRepository.deleteById(id);
     }
 }
---
+++ src/main/java/com/anime/streamingserver/service/anime/FavoriteService.java
@@ -0,0 +1,4 @@
+package com.anime.streamingserver.service.anime;
+
+public class FavoriteService {
+}
